94ae26e609fdba78b18fcb2a8a6994642d5f01fe
SreekarSBS <sbssreekar@gmail.com>
2025-07-01 21:11:13 +0530

    Deploy

--- next.config.ts
+++ next.config.ts
@@ -11,5 +11,6 @@ const config: NextConfig = {
     ],
   },
 }
- 
+
+
 export default config
---
+++ src/utils/firebase.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD4Vw_ANTEylWpH9kPymTkW4S7en-45r04",
+  authDomain: "bingebot-1cf35.firebaseapp.com",
+  projectId: "bingebot-1cf35",
+  storageBucket: "bingebot-1cf35.firebasestorage.app",
+  messagingSenderId: "687304622457",
+  appId: "1:687304622457:web:1fa4d5a1ed192c26202fa8",
+  measurementId: "G-64CKZW0SS0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const analytics = getAnalytics(app);
