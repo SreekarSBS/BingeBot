1426a745969b3bc1ce5f5b6f0036bb8b9650180d
SreekarSBS <sbssreekar@gmail.com>
2025-07-02 18:58:48 +0530

    responsiveness for xl and 2xl

--- src/app/SignIn/page.tsx
+++ src/app/SignIn/page.tsx
@@ -29,23 +29,23 @@ import { useEffect, useRef, useState } from "react";
     }
      const path = !showPassword? "https://www.svgrepo.com/show/348139/eye-crossed.svg" : "https://www.svgrepo.com/show/365364/eye-thin.svg";
     
- return <div>
+ return <div className="relative w-full h-screen overflow-hidden">
 
-        <Image className="mask-t-from-background mask-b-from-background " fill = {true}  src= "https://assets.nflxext.com/ffe/siteui/vlv3/75b0ed49-75ab-4a63-bd45-37bc2c95cb73/web/IN-en-20250623-TRIFECTA-perspective_ae5833b7-6ce5-4e88-853e-014f38c506f1_large.jpg" alt = "Netflix-bg" />
+        <Image className="mask-t-from-background mask-b-from-background " fill src= "https://assets.nflxext.com/ffe/siteui/vlv3/75b0ed49-75ab-4a63-bd45-37bc2c95cb73/web/IN-en-20250623-TRIFECTA-perspective_ae5833b7-6ce5-4e88-853e-014f38c506f1_large.jpg" alt = "Netflix-bg" />
               
-         <div className="w-3/12 mx-auto left-0 right-0 my-auto top-0 bottom-0 absolute h-[800px] rounded-3xl text-white bg-black/60  ">
-          <div className=" w-3/4 h-8/12 m-12 p-12 ">
-              <div className="text-4xl font-light font-serif  ">Sign { isSignup? 'Up' : 'In'}</div>
-              <form onSubmit={(e) => e.preventDefault()} className=" text-white py-12 text-2xl" >
-                <input ref = {email} type = "text" placeholder="Email address " className="bg-gray-700 w-96 rounded-lg h-18 my-4 p-2 text-white  "  />
-                <div className="flex items-center w-96">
-                <input ref = {password} type = {`${showPassword ? "text" : "password" }`} placeholder="Password" className="w-full  rounded-lg h-18 my-4 p-2 text-white bg-gray-700" />
-                <div onClick={handleClick} className="p-4 hover:bg-red-600 bg-gray-50 cursor-pointer rounded-4xl ml-84 my-4 border-2 absolute">
+         <div className="2xl:w-3/12 xl:w-96 mx-auto left-0 right-0 my-auto top-0 bottom-0 absolute xl:h-[510px] 2xl:h-[800px] rounded-3xl text-white bg-black/60  ">
+          <div className=" w-3/4 h-8/12 xl:m-6 xl:p-2 2xl:m-12  2xl:p-12 ">
+              <div className="2xl:text-3xl xl:text-2xl font-light 2xl:mt-0 xl:mt-6 font-serif  ">Sign { isSignup? 'Up' : 'In'}</div>
+              <form onSubmit={(e) => e.preventDefault()} className="relative xl:w-80 2xl:w-96 text-white xl:py-8 2xl:py-12 2xl:text-2xl" >
+                <input ref = {email} type = "text" placeholder="Email address " className="bg-gray-700 xl:w-80 2xl:w-96 rounded-lg xl:h-14 2xl:h-18 my-4 p-2 text-white  "  />
+                <div className="flex items-center xl:w-80 2xl:w-96">
+                <input ref = {password} type = {`${showPassword ? "text" : "password" }`} placeholder="Password" className="w-full rounded-lg xl:h-14 2xl:h-18 my-4 p-2 text-white bg-gray-700" />
+                <div onClick={handleClick} className="p-4 hover:bg-red-600 bg-gray-50 cursor-pointer rounded-4xl 2xl:ml-84 my-4 border-2 right-2 absolute">
                   <Image fill = {true} src={path} alt = "eye-svg"  />
                    </div>
                 </div>  
                 <p className="font-bold text-xl text-red-600 ">{errorMessage}</p>
-                <button onClick={handleSign} className="w-96 h-18 my-6 cursor-pointer rounded-lg bg-red-600 hover:text-black ">Sign In</button>
+                <button onClick={handleSign} className="2xl:w-96 xl:w-80 xl:h-14 2xl:h-18 my-6 cursor-pointer rounded-lg bg-red-600 hover:text-black ">Sign In</button>
               
                   <div className="flex mt-6"> New to Netflix? <Link href="/"> <p className="cursor-pointer hover:text-red-600 mx-1 font-light hover:underline">Sign up </p></Link></div>
              
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,32 +3,32 @@ import Link from "next/link"
 
 const SignUp = () => {
   return (
-    <div>
+    <div className="relative w-full h-screen bg-black overflow-hidden">
       <Link href="/SignIn">
-       <button className=" w-28 h-14 m-8 cursor-pointer font-serif hover:text-white text-red-50 hover:bg-violet-900/30 duration-1000 rounded-xl  absolute z-50 right-96 bg-violet-900/80 text-shadow-black text-xl  font-bold " >
+       <button className=" w-28 h-14 m-8 xl:right-8 cursor-pointer font-serif hover:text-white text-red-50 hover:bg-violet-900/30 duration-1000 rounded-xl  absolute z-50 2xl:right-96 bg-violet-900/80 text-shadow-black text-xl  font-bold " >
         
           Sign In
           
           </button>
           </Link>
        <Image className="mask-t-from-background mask-b-from-background " fill = {true}  src= "https://assets.nflxext.com/ffe/siteui/vlv3/75b0ed49-75ab-4a63-bd45-37bc2c95cb73/web/IN-en-20250623-TRIFECTA-perspective_ae5833b7-6ce5-4e88-853e-014f38c506f1_large.jpg" alt = "Netflix-bg" />
-             <div className="w-1/2 h-3/6  mx-auto my-auto left-0 absolute top-0 bottom-16 right-0 z-50 ">
-                <div className="mx-28 mt-24 h-2/4 p-12   text-center  ">
+             <div className="2xl:w-3/6 xl:w-full  h-3/6  mx-auto my-auto left-0 absolute top-0 bottom-0 right-0  ">
+                <div className="mx-28 xl:mt-14 2xl:mt-24 h-2/4  text-center  ">
 
-                 <div className="text-7xl font-serif  text-white   font-black ">
+                 <div className="2xl:text-7xl xl:text-5xl font-serif  text-white xl:font-semibold  2xl:font-black ">
                    Unlimited Movies, TV Shows and more
               </div>
-                <div className="text-3xl text-gray-400 my-12 font-extrabold">
+                <div className="2xl:text-3xl  xl:text-2xl text-gray-400 my-12 xl:font-semibold 2xl:font-extrabold">
                   Starts at $149 . Cancel at any time .
                 </div>
-                <div className="text-2xl my-6 font-light font-serif ">
+                <div className="2xl:text-2xl xl:text-xl my-6 font-light font-serif ">
                 Ready to watch? Enter your email to create or restart your membership.
                 </div>
                 <div className="flex  ">
                  
-                  <input type = "text" placeholder="Email address" className="bg-gray-700 mr-2 w-2/3 rounded-lg h-18 my-4 p-2 text-white relative" />
+                  <input type = "text" placeholder="Email address" className="bg-gray-700 mr-2 w-2/3 rounded-lg xl:h-14 2xl:h-18 my-4 p-2 text-white relative" />
                   
-                  <button className=" w-1/3 ml-2  h-18 my-4 p-2 cursor-pointer font-serif hover:text-white text-red-50 hover:bg-red-900/30 duration-1000 rounded-xl   z-50 right-96 bg-red-900/80 text-shadow-black text-xl  font-bold " >
+                  <button className=" w-1/3 ml-2 xl:h-14 2xl:h-18 my-4 p-2 cursor-pointer font-serif hover:text-white text-red-50 hover:bg-red-900/30 duration-1000 rounded-xl   z-50 right-96 bg-red-900/80 text-shadow-black text-xl  font-bold " >
         Get Started
         </button>
                 </div>
