a1869ffe65e78ee2b143da7b9cdfcc2ff80be89c
SreekarSBS <sbssreekar@gmail.com>
2025-08-21 20:06:31 +0530

    Favicon Changes

--- src/components/GptSearchBar.jsx
+++ src/components/GptSearchBar.jsx
@@ -10,7 +10,7 @@ const GptSearchBar = () => {
   return (
     
    
-    <div className=" pt-[10%] sm:pt-[5%]  flex flex-col items-center">
+    <div className="   flex flex-col items-center">
     <h1 className="text-xl md:text-2xl lg:text-3xl xl:text-4xl flex flex-col text-pink-600 mt-12 font-black underline-offset-8 underline z-50 py-12 font-stretch-125% ">Search Recommendations</h1>
       <form onSubmit={(e) => e.preventDefault()} className="gap-2 w-3/4 m-4 sm:w-3/4 lg:w-1/2 sm:m-0   h-12 grid grid-cols-12">
         <input ref = {searchText} placeholder={lang[selectedLanguage].searchPlaceholder } className="rounded-lg p-4 border col-span-9"></input>
